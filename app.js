fe8fa0e02d7dfeb58ece6d42a9092a52316e36c6
AbdulRehman <likeam99@gmail.com>
2022-06-12 17:41:50 +0500

    inser col

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ function Book(title, author, isbn){
 //UI Constructor
 function UI(){}
 
+//Add book to list
+UI.prototype.addBookToList = function(book){
+
+    const list = document.getElementById('book -list');
+
+    //Create tr Element
+    const row = document.createElement('tr');
+    console.log(book.title);
+
+    //Insert cols
+    row.innerHTML = `'<td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href='#' class="delete">X</a></td>  '  
+    `;
+    list.appendChild(row);
+}
+// Clear Fields
+UI.prototype.clearFields = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
 
 //Event Listiner
 document.getElementById('book-form').addEventListener('submit', function(e){
@@ -18,7 +41,15 @@ document.getElementById('book-form').addEventListener('submit', function(e){
         isbn = document.getElementById('isbn').value;
     //Instantiate Book
     const book = new Book(title, author, isbn);
-        console.log(book);
+    
+    //Instanatiate UI
+    const ui = new UI();
+
+    //Add book to list
+    ui.addBookToList(book);
+
+    //Clear fields
+    ui.clearFields();
 
 
     e.preventDefault();
